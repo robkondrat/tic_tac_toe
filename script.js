5545a02be213b122a314d9accd07d4e44c17256f
EatYourSoup7 <r.kondratowicz1@gmail.com>
2021-05-19 17:14:15 -0500

    draw scenario and play again button reloads page

--- script.js
+++ script.js
@@ -42,8 +42,8 @@ function clickedBox(element) {
     element.setAttribute("id", playerSign);
   }
   selectWinner(); // call the winner function
-  playBoard.style.pointerEvents = "none"; 
   element.style.pointerEvents = "none"; // once user selects any box then that box can't be selected again
+  playBoard.style.pointerEvents = "none"; 
   let randomDelayTime = ((Math.random() * 1000) + 300).toFixed(); // generating random time delay so bot will delay randomly to select box
   setTimeout(() => {
     bot(runBot); // calling bot function
@@ -51,9 +51,9 @@ function clickedBox(element) {
 }
 
 function bot(runBot) {
+  let array = []; // creating empty array where we'll store unselected boxes
   if (runBot) {
     playerSign = "O";
-    let array = []; // creating empty array where we'll store unselected boxes
     for (let i = 0; i < allBox.length; i++) {
       if (allBox[i].childElementCount == 0) { 
         array.push(i);
@@ -92,7 +92,6 @@ function checkThreeIds(val1, val2, val3, sign) {
 
 function selectWinner() { // if one combination of these return true then select the winner
   if (checkThreeIds(1,2,3,playerSign) || checkThreeIds(4,5,6,playerSign) || checkThreeIds(7,8,9,playerSign) || checkThreeIds(1,4,7,playerSign) || checkThreeIds(2,5,8,playerSign) || checkThreeIds(3,6,9,playerSign) || checkThreeIds(1,5,9,playerSign) || checkThreeIds(3,5,7,playerSign)){
-    // console.log(playerSign + " " + "is the winner");
     // when the match is won stop the bot
     runBot = false;
     bot(runBot);
@@ -102,6 +101,23 @@ function selectWinner() { // if one combination of these return true then select
     }, 700);
 
     // show the result box with the the winner
-    wonText.innerHTML = `Player <p>${playerSign}</p> wins!`
+    wonText.innerHTML = `Player <p>${playerSign}</p> wins!`;
+  } else {
+    // draw scenario
+    if (getId(1) != "" && getId(2) != "" && getId(3) != "" && getId(4) != "" && getId(5) != "" && getId(6) != "" && getId(7) != "" && getId(8) != "" && getId(9) != "") {
+      runBot = false;
+      bot(runBot);
+      setTimeout(() => { // delay to show result box
+        playBoard.classList.remove("show");
+        resultBox.classList.add("show");
+      }, 700);
+  
+      // show the result box with the the winner
+      wonText.textContent = `It's a draw!`;
+    }
   }
+}
+
+replayBtn.onclick = () => {
+  window.location.reload(); // reload the current page;
 }
